fabb3b5f187471fa7b5fdf7f0f52e1aa5dee63bb
JorryZ <45359771+JorryZ@users.noreply.github.com>
2019-11-20 10:58:00 +0800

    Create __init__.py

---
+++ strain3D/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 10:56:00 2019
+
+@author: JorryZ
+
+File: __init__.py
+Description: load strainMyocardium
+
+History:
+    Date    Programmer SAR# - Description
+    ---------- ---------- ----------------------------
+  Author: jorry.zhengyu@gmail.com         20NOV2019             -V1.0.0 test version
+                                                        -strainMyocardium version 1.0.0                                            
+                                                   
+Requirements:
+    numpy
+    scipy
+    motionSegmentation
+    medImgProc
+    trimesh
+    json (optional)
+    pickle (optional)
+All rights reserved.
+"""
+_version='1.0.0'
+print('strain3D version',_version)
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+import strain3D.strainMyocardium as strainMyocardium
